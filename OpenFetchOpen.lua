e427c53d012badf8981fff8595b084b785f6ce40
AngoNex <71973953+AngoNex@users.noreply.github.com>
2021-10-09 17:05:53 +0300

    Update OpenFetchOpen.lua

--- OpenFetchOpen.lua
+++ OpenFetchOpen.lua
@@ -1,4 +1,5 @@
 local shell = require("shell")
+local term = require("term")
 term.clear()
 shell.execute("cd //")
 local seccess, reason = pcall(loadfile("bin/openfetch.lua")); if not seccess then print("Ошибка запуска OpenFetch :"..tostring(reason)) end
